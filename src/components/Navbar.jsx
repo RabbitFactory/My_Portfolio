f5b2df5be92588f327a735732c403459224f28c3
Solaiman Hossain <jv16th.raj@gmail.com>
2025-02-26 16:17:53 +0600

    added text to nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,7 @@ const Navbar = () => {
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Link to="/dashboard">Dashboard</Link>
+      <Link to="/text">text</Link>
     </nav>
   )
 }
